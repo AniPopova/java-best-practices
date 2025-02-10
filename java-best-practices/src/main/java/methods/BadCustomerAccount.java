a559679332b0c8d54fbfbb1aa5413dc715d9c088
Ani Popova <ana.popova83@gmail.com>
2025-02-10 09:44:20 +0200

    Sections Three - Methods rules.

---
+++ java-best-practices/src/main/java/methods/BadCustomerAccount.java
@@ -0,0 +1,31 @@
+package methods;
+
+import java.math.BigDecimal;
+
+public class BadCustomerAccount {
+    private final String id;
+    private BigDecimal balance;
+
+    public BadCustomerAccount(String id, BigDecimal balance) {
+        this.id = id;
+        this.balance = balance;
+    }
+
+    public BigDecimal processTransaction(String transactionType, BigDecimal amount) {
+        if (transactionType.equals("deposit")) {
+            this.balance = this.balance.add(amount);
+            System.out.println("Deposit $" + amount);
+            return balance;
+        } else if (transactionType.equals("withdraw")) {
+            if (amount.compareTo(this.balance) > 0) {
+                throw new IllegalArgumentException("Insufficient balance! balance = " + this.balance + " , amount = " + amount);
+            }
+            this.balance = this.balance.subtract(amount);
+            System.out.println("Withdraw $" + amount);
+            return balance;
+        } else {
+            System.out.println("Invalid transaction type.");
+            return balance;
+        }
+    }
+}
---
+++ java-best-practices/src/main/java/methods/Responsibility.java
@@ -0,0 +1,35 @@
+package methods;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+public class Responsibility {
+    /**
+     * One method should do one of the following two tings:
+     * (CQS) Command-query separation
+     * 1. Command: modify the state of an object and return nothing (void);
+     * 2. Query: perform computation and return its result;
+     * 3. the method should avoid any undocumented side effects
+     */
+
+    public static void main(String[] args) {
+        System.out.println("-- BAD PRACTICE --");
+        showBadPractice();
+
+        System.out.println("\n-- GOOD PRACTICE --");
+        showGoodPractice();
+    }
+
+    private static void showBadPractice() {
+        BadCustomerAccount account = new BadCustomerAccount(UUID.randomUUID().toString(), BigDecimal.valueOf(100.0));
+        account.processTransaction("withdraw", BigDecimal.valueOf(10.0));
+        account.processTransaction("deposit", BigDecimal.valueOf(50.0));
+    }
+
+    private static void showGoodPractice() {
+        GoodCustomerAccount account = new GoodCustomerAccount(UUID.randomUUID().toString(), BigDecimal.valueOf(100.0));
+        account.withdraw(BigDecimal.valueOf(10.0));
+        account.deposit(BigDecimal.valueOf(50.0));
+    }
+
+}
